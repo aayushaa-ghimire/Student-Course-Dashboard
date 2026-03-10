d2cce297142b9c2bed92030bee31e10b9f012141
Aayusha <ayushaghimireg@gmail.com>
2026-03-10 12:03:36 +0545

    Home Page Completed

--- Project7/src/Pages/Home.jsx
+++ Project7/src/Pages/Home.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import Courses from '../Pages/Courses';
+
 
 function Home() {
   return (
     <>
     <div>
-        
+        <h1>Welcome to the Home Page</h1>
+        <Router>
+          <ul>
+            <li><Link to={'/courses'}></Link></li>
+          </ul>
+          
+          <Routes>
+             <Route path='/courses' Component={Courses}></Route>
+          </Routes>
+        </Router>
     </div>
     </>
   )
