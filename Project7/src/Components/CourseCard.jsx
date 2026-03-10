c3243c3db9016f6a21907519f3a482553fcc95d5
Aayusha <ayushaghimireg@gmail.com>
2026-03-10 12:24:38 +0545

    CourseCard Completed

--- Project7/src/Components/CourseCard.jsx
+++ Project7/src/Components/CourseCard.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-
-function CourseCard() {
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Courses from '../Pages/Courses'
+function CourseCard({course}) {
   return (
     <>
     <div>
-        
+        <h3>{course.name}</h3>
+        <p>Level: {course.level}</p>
+        <Router>
+            <ul>
+                <li><Link to="/courses"> </Link></li>
+            </ul>
+
+            <Routes>
+                <Route path='/courses' Component={Courses}></Route>
+            </Routes>
+        </Router>
+
     </div>
     </>
   )
